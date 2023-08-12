f115557a3b8f7fb33a38a73f2da35f92aa60afb0
Iqomuddin <abdullah180102@gmail.com>
2023-08-12 21:06:21 +0700

    UAS AKB 2023

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/AbdullahTsaniIqomuddin_10120171_IF5/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.AbdullahTsaniIqomuddin_10120171_IF5.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.AbdullahTsaniIqomuddin_10120171_IF5.test";
+  public static final String BUILD_TYPE = "debug";
+}
