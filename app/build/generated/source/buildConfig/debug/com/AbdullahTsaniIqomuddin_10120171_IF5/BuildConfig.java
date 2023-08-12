659f3f85caa49cec121181af7686cbe707343066
Iqomuddin <abdullah180102@gmail.com>
2023-08-12 20:19:38 +0700

    2023

---
+++ app/build/generated/source/buildConfig/debug/com/AbdullahTsaniIqomuddin_10120171_IF5/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.AbdullahTsaniIqomuddin_10120171_IF5;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.AbdullahTsaniIqomuddin_10120171_IF5";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
